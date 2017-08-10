fc6fa37c71d2f2209b09bcfdee2557336015bd5b
Kevin Martin <kevinbreaker2604@gmail.com>
2017-08-10 08:51:28 -0300

    Em progresso...

--- TyperGame/public/js/frase.js
+++ TyperGame/public/js/frase.js
@@ -1,4 +1,15 @@
-$("#botao-frase").click(()=>{
+$("#botao-frase").click(fraseAleatoria);
+
+function fraseAleatoria() {
+  $.get("http://localhost:3000/frases",trocaAleatoria);
+
+}
+
+function trocaAleatoria(data) {
+
+  let frase = $(".frase");
+  let numAleatorio = Math.floor(Math.random() * data.length);
 
 
-});
+  frase.text(data[numAleatorio].texto);
+}
